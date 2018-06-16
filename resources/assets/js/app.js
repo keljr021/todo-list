42d6e3952461702bd82d08a1404be797860a9177
unknown <keljr021@gmail.com>
2018-06-16 18:21:27 -0400

    Was able to properly install Bootstrap. Building view for task list.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -20,3 +20,12 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 const app = new Vue({
     el: '#app'
 });
+
+function viewTaskItem(id) {
+
+  location.href = "/task/" + id;
+}
+
+$('tr').click(function() {
+  viewTaskItem($(this).attr('data-id'));
+});
--- resources/views/tasks/list.blade.php
+++ resources/views/tasks/list.blade.php
@@ -1,13 +1,43 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+      <title>List of Tasks</title>
+      <link rel="stylesheet" href="{{{ URL::asset('css/app.css')}}}" />
+      <script type="text/javascript" src="{{{ URL::asset('js/app.js')}}}"></script>
     </head>
     <body>
-      <h3>List of Tasks</h3>
-      <ul>
-      @foreach($tasks as $task)
-        <li>{{ $task->id }} <a href="/tasks/{{ $task->id }}">{{ $task->title }}</a></li>
-      @endforeach
-    </ul>
+      <div class="container">
+        <h3>List of Tasks</h3>
+
+        <div class="tablebox">
+          <table class="table table-responsive table-hover table-striped">
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Priority</th>
+              <th>Completed?</th>
+              <th>Created By</th>
+              <th>&nbsp;</th>
+            </tr>
+            @foreach($tasks as $task)
+              <tr data-id="{{ $task->id }}">
+                <td>{{ $task->id }}</td>
+                <td>{{ $task->title }}</td>
+                <td>{{ $task->priority_ids }}</td>
+                <td>
+                  <input type="checkbox" value="true" {{ ($task->is_completed) ? 'checked' : '' }} />
+                </td>
+                <td>{{ $task->created_at }}</td>
+                <td>[buttons here]</td>
+              </tr>
+            @endforeach
+          </table>
+        </div>
+
+        <div>
+          <button class="button">+ Add</button>
+        </div>
+
+      </div>
     </body>
 </html>
