a3c8a86741f19c19ff3d2e39ff37f9799b870a26
unknown <keljr021@gmail.com>
2018-06-16 16:42:34 -0400

    Modified Task factory to add priority IDs and complete status.

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -6,6 +6,7 @@ $factory->define(App\Task::class, function (Faker $faker) {
     return [
         'title' => $faker->realText($maxNbChars = 100),
         'body'  => $faker->text($maxNbChars = 100),
-        'priority_ids' => null,
+        'priority_ids' => json_encode([rand(1,4)]),
+        'is_completed'  => rand(0,1),
     ];
 });
