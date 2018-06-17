33f46c9db2965c3df277113260bff25574e8f315
unknown <keljr021@gmail.com>
2018-06-17 12:51:59 -0400

    Added Edit and Delete buttons, modified table structure.

---
+++ public/js/list.js
@@ -0,0 +1,21 @@
+function editTaskItem(id) {
+
+  location.href = "/tasks/" + id;
+
+}
+
+$(function() {
+
+  $('.editBtn').click(function() {
+    let id = $(this).parent().parent().attr('data-id');
+
+    editTaskItem(id);
+  });
+
+  $('.deleteBtn').click(function() {
+    let id = $(this).parent().parent().attr('data-id');
+
+    console.log('delete item ', id);
+  });
+
+});
--- resources/views/tasks/list.blade.php
+++ resources/views/tasks/list.blade.php
@@ -3,42 +3,51 @@
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="csrf-token" content="{{ csrf_token() }}">
 
       <title>List of Tasks</title>
       <link rel="stylesheet" href="{{{ URL::asset('css/app.css')}}}" />
       <script type="text/javascript" src="{{{ URL::asset('js/app.js')}}}"></script>
+
+      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+      <script type="text/javascript" src="{{{ URL::asset('js/list.js')}}}"></script>
     </head>
     <body>
-      <div class="container">
+      <div class="container-fluid">
         <h3>List of Tasks</h3>
 
         <div class="tablebox">
-          <table class="table table-responsive table-hover table-striped">
-            <tr>
-              <th>#</th>
-              <th>Title</th>
-              <th>Priority</th>
-              <th>Completed?</th>
-              <th>Created By</th>
-              <th>&nbsp;</th>
-            </tr>
+          <table class="table table-hover table-striped">
+            <thead class="thead-dark">
+              <tr>
+                <th>&nbsp;</th>
+                <th>Title</th>
+                <th>Priority</th>
+                <th>Created By</th>
+                <th>&nbsp;</th>
+              </tr>
+            </thead>
+            <tbody>
             @foreach($tasks as $task)
               <tr data-id="{{ $task->id }}">
-                <td>{{ $task->id }}</td>
-                <td>{{ $task->title }}</td>
-                <td>{{ $task->priority_ids }}</td>
                 <td>
                   <input type="checkbox" value="true" {{ ($task->is_completed) ? 'checked' : '' }} />
                 </td>
+                <td>{{ $task->title }}</td>
+                <td>{{ $task->priority_ids }}</td>
                 <td>{{ $task->created_at }}</td>
-                <td>[buttons here]</td>
+                <td>
+                  <button type="button" class="btn btn-outline-secondary btn-sm editBtn">Edit</button>
+                  <button type="button" class="btn btn-outline-secondary btn-sm deleteBtn">Delete</button>
+                </td>
               </tr>
             @endforeach
+            </tbody>
           </table>
         </div>
 
         <div>
-          <button class="button">+ Add</button>
+          <button class="btn btn-outline-secondary">+ Add</button>
         </div>
 
       </div>
